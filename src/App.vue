e656841d1323c976956c750e879108e6a58620f7
sbhongade598@gmail.com <sbhongade598@gmail.com>
2022-10-03 19:38:31 +0900

    Initial setting

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  export default{
+    name : 'App'
+  }
+
+</script>
+
+<template>
+  <h1>Web form</h1>
+  
+</template>
+
+<style >
+  body{
+    margin: 0;
+  }
+  h1{
+    text-align: center;
+  }
+</style>
